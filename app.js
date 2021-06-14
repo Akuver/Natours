9c36b4da970b49cae5cef3a24a1d5163b2e083cc
Akuver <aanjaneykumar.verma@gmail.com>
2021-06-14 12:07:54 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -23,6 +25,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 //1) middleware
+//Implement CORS
+app.use(cors());
+app.options('*', cors());
+
 // Set security HTTP headers
 app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
